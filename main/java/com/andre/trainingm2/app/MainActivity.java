1687c71b7320d35320d7c3db34de60b9e456bb13
andre hardian faisal <andre@neo-fusion.com>
2015-01-05 16:19:50 +0700

    Project Training M2,insert data only but error in modeldata.getpict

---
+++ main/java/com/andre/trainingm2/app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.andre.trainingm2.app;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.andre.trainingm2.app.fragment.NewContact;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.Utama,new NewContact()).commit();
+    }
+
+
+}
